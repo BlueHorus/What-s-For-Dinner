35e859a66751430a79fb710516386014a8fc4523
Patrick Lewis <lewispjjpb@gmail.com>
2021-12-08 07:29:10 -0500

    INITIAL TEMPLATE CREATED

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+
+
+class Main extends React.Component {
+
+
+  render() {
+    return(
+      <h1>Hello Blue Ocean World!</h1>
+    )
+  }
+}
+
+
+export default Main
