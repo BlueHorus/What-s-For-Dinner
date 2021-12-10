4bc3242317a56a929ba5ab7231a66ac0b4487486
Patrick Lewis <lewispjjpb@gmail.com>
2021-12-10 12:07:38 -0500

    Lowercase S at end of SVGS filepath reference

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 import Featured from './featured/featured.js';
 import Recipe from './shared/recipecard.js';
 import searchIcon from './shared/SVGS/SearchIcon.svg';
-import ingredientIcon from './shared/SVGs/IngredientIcon.svg'
-import recipeIcon from './shared/SVGs/recipesIcon.svg';
+import ingredientIcon from './shared/SVGS/IngredientIcon.svg'
+import recipeIcon from './shared/SVGS/recipesIcon.svg';
 import profileIcon from './shared/SVGS/profileIcon.svg';
 
 
