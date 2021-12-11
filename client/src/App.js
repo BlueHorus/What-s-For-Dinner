a753ac8914a9c4f85ef4a0b1c604a47cb40ef3e5
James Plier <plier2j@gmail.com>
2021-12-11 17:42:58 -0500

    Change default page to landing, add link to title

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,6 @@ import React from "react";
 import axios from "axios";
 
 // Page Imorts
-<<<<<<< HEAD
-import Featured from './featured/featured.js';
-import Recipe from './shared/recipecard.js';
-import searchIcon from './shared/SVGS/SearchIcon.svg';
-import Landing from './landing/landing.js'
-import title from '../../public/images/title.svg'
-import ingredientIcon from './shared/SVGS/IngredientIcon.svg'
-import recipeIcon from './shared/SVGS/recipesIcon.svg';
-import profileIcon from './shared/SVGS/profileIcon.svg';
-import Ingredients from './ingredients/ingredients.js'
-import MyProfile from './MyProfile.jsx';
-
-
-=======
 import Featured from "./featured/featured.js";
 import Recipe from "./shared/recipecard.js";
 import searchIcon from "./shared/SVGS/SearchIcon.svg";
@@ -26,15 +12,15 @@ import profileIcon from "./shared/SVGS/profileIcon.svg";
 import Ingredients from "./ingredients/ingredients.js";
 import MyProfile from "./MyProfile.jsx";
 import Auth from "./auth/login.jsx";
+import title from '../../public/images/title.svg'
 import { app } from "../../firebase_config.js";
 import { getAuth, onAuthStateChanged, updateCurrentUser } from "firebase/auth";
->>>>>>> dev
 
 class Main extends React.Component {
   constructor() {
     super();
     this.state = {
-      id: "login-signup",
+      id: "landing",
       user: sampleUser,
       intolerances: "",
       diet: "",
@@ -105,28 +91,17 @@ class Main extends React.Component {
     console.log(recipeId);
     let id = recipeId.toString();
     switch (event.target.className) {
-<<<<<<< HEAD
-      case 'upvote-button':
-=======
       case "upvote-button":
         console.log("test upvote");
->>>>>>> dev
         ((recipeId) => {
           let config = {
             method: "put",
             url: "/updateUpvote",
             data: {
-<<<<<<< HEAD
               recipeId: id
             }
           }
           axios(config)
-=======
-              recipeId: recipeId,
-            },
-          };
-          axios(config);
->>>>>>> dev
         })();
         break;
       case "downvote-button":
@@ -136,17 +111,10 @@ class Main extends React.Component {
             method: "put",
             url: "/updateDownvote",
             data: {
-<<<<<<< HEAD
               recipeId: id
             }
           }
           axios(config)
-=======
-              recipeId: recipeId,
-            },
-          };
-          axios(config);
->>>>>>> dev
         })();
         break;
       default:
@@ -217,22 +185,6 @@ class Main extends React.Component {
           <span id="landing" className="logo" onClick={this.viewSwitch} />
           <img id="title" src={title}/>
           <div id="find-recipes" onClick={this.viewSwitch}>
-<<<<<<< HEAD
-            <img  width='30' src={searchIcon}></img>
-             <span>Find Recipes</span>
-          </div>
-          <div id="my-ingredients" onClick={this.viewSwitch}>
-            <img width='30' src={ingredientIcon} />
-            <span>My Ingredients</span>
-          </div>
-          <div id="my-recipes" onClick={this.viewSwitch}>
-            <img width='30' src={recipeIcon} />
-            <span>My Recipes</span>
-          </div>
-          <div id="login-signup" onClick={this.viewSwitch}>
-            <img width='30' src={profileIcon} />
-            <span>Profile</span>
-=======
             <img width="30" src={searchIcon}></img>
             Find Recipes
           </div>
@@ -247,7 +199,6 @@ class Main extends React.Component {
           <div id="login-signup" onClick={this.viewSwitch}>
             <img width="30" src={profileIcon} />
             Profile
->>>>>>> dev
           </div>
           <Auth status={this.getStatus} login={this.state.login} />
         </div>
--- client/src/index.js
+++ client/src/index.js
@@ -2,23 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./App.js";
 
-<<<<<<< HEAD
-
-// firebase.initializeApp({
-//   apiKey: "AIzaSyDfjkH0RspPPEb3OBQs0emN1yZgSb-LDrA",
-//   authDomain: "blue-ocean-b857a.firebaseapp.com",
-//   projectId: "blue-ocean-b857a",
-//   storageBucket: "blue-ocean-b857a.appspot.com",
-//   messagingSenderId: "226119114824",
-//   appId: "1:226119114824:web:2b3e396931470d5e8ef87d",
-//   measurementId: "${config.measurementId}",
-// });
-
-
-ReactDOM.render(
-  <Main/>,
-  document.getElementById('root')
-);
-=======
 ReactDOM.render(<Main />, document.getElementById("root"));
->>>>>>> dev
