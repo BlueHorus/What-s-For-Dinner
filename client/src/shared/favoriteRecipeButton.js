d8b176a62ead13f3d33a817dda8c6eba73a6066d
Xiaohan Liu <danae0730@gmail.com>
2021-12-16 07:54:16 -0800

    Handle no user

--- client/src/shared/favoriteRecipeButton.js
+++ client/src/shared/favoriteRecipeButton.js
@@ -1,40 +1,61 @@
-import React from 'react'
-import IconButton from '@mui/material/IconButton';
-import StarOutlineIcon from '@mui/icons-material/StarOutline';
-import Tooltip from '@mui/material/Tooltip';
-import StarIcon from '@mui/icons-material/Star';
+import React from "react";
+import IconButton from "@mui/material/IconButton";
+import StarOutlineIcon from "@mui/icons-material/StarOutline";
+import Tooltip from "@mui/material/Tooltip";
+import StarIcon from "@mui/icons-material/Star";
 
 const FavoriteButton = (props) => {
   let recipeId = props.recipe.id.toString();
   if (props.user === null) {
     return (
-      <IconButton id='favorite-button'  onClick={() => alert('please sign up or login to use this feature')} size='large' >
-        <StarOutlineIcon style={{width: '20px', height: '20px'}} nClick={() => alert('please sign up or login to use this feature')}/>
+      <IconButton
+        id="favorite-button"
+        onClick={() => alert("please sign up or login to use this feature")}
+        size="large"
+      >
+        <StarOutlineIcon
+          style={{ width: "20px", height: "20px" }}
+          nClick={() => alert("please sign up or login to use this feature")}
+        />
       </IconButton>
-      )
-    }
-  if (props.user.favoriteRecipes.indexOf(recipeId) === -1) {
-  return (
-    <Tooltip title="Add to Favorites">
-      <IconButton id='favorite-button' size='large'  onClick={() => props.handleButtonPress(props.recipe.id)}>
-        <StarOutlineIcon id='favorite-button' style={{width: '20px', height: '20px'}} onClick={() => props.handleButtonPress(props.recipe.id)}/>
-      </IconButton>
-    </Tooltip>
-    )
- } else {
-   return (
-     <Tooltip title="Remove Favorite">
-       <IconButton id='unfavorite-button' size='large'  onClick={() => props.handleButtonPress(props.recipe.id)}>
-        <StarIcon id='unfavorite-button' style={{width: '20px', height: '20px'}} onClick={() => props.handleButtonPress(props.recipe.id)}/>
-      </IconButton>
-     </Tooltip>
-   )
- }
-}
+    );
+  }
+  if (
+    props.user.favoriteRecipes.indexOf(recipeId) === -1 ||
+    props.user.favoriteRecipes === undefined
+  ) {
+    return (
+      <Tooltip title="Add to Favorites">
+        <IconButton
+          id="favorite-button"
+          size="large"
+          onClick={() => props.handleButtonPress(props.recipe.id)}
+        >
+          <StarOutlineIcon
+            id="favorite-button"
+            style={{ width: "20px", height: "20px" }}
+            onClick={() => props.handleButtonPress(props.recipe.id)}
+          />
+        </IconButton>
+      </Tooltip>
+    );
+  } else {
+    return (
+      <Tooltip title="Remove Favorite">
+        <IconButton
+          id="unfavorite-button"
+          size="large"
+          onClick={() => props.handleButtonPress(props.recipe.id)}
+        >
+          <StarIcon
+            id="unfavorite-button"
+            style={{ width: "20px", height: "20px" }}
+            onClick={() => props.handleButtonPress(props.recipe.id)}
+          />
+        </IconButton>
+      </Tooltip>
+    );
+  }
+};
 
-
-
-
-
-
-export default FavoriteButton
+export default FavoriteButton;
